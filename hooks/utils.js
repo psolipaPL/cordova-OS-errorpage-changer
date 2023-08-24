52df022e2039733165063f58c1c69a472e1f4b1a
Paulo Solipa <psolipa@product-league.com>
2023-08-24 09:24:22 +0100

    Revert all changes

--- hooks/utils.js
+++ hooks/utils.js
@@ -5,7 +5,7 @@ const configs = {
     textToReplace: 'There was an error processing your request.',
     androidPath: "/platforms/android/app/src/main/assets/www/",
     iosPath: "/platforms/ios/www/",
-    errorFile: 'index.html'
+    errorFile: '_error.html'
 };
 
 function getConfigs() {
@@ -21,31 +21,8 @@ function errorFileReplacer(path, content, textToReplace, replacementText) {
     fs.writeFileSync(path, content, "utf-8");
 }
 
-function indexReplacer(path, content) {
-    content = content.replace('<script type="text/javascript" src="scripts/OutSystemsManifestLoader.js', '<script async type="text/javascript" src="scripts/OutSystemsManifestLoader.js');
-    //content = content.replace('<script type="text/javascript" src="scripts/OutSystems.js', '<script async type="text/javascript" src="scripts/OutSystems.js');
-    content = content.replace('<script type="text/javascript" src="scripts/OutSystemsReactView.js', '<script async type="text/javascript" src="scripts/OutSystemsReactView.js');
-    content = content.replace('<script type="text/javascript" src="scripts/cordova.js', '<script async type="text/javascript" src="scripts/cordova.js');
-    content = content.replace('<script type="text/javascript" src="scripts/Debugger.js', '<script async type="text/javascript" src="scripts/Debugger.js');
-    content = content.replace('<script type="text/javascript" src="scripts/ECOP_Mobile_PS.appDefinition.js', '<script async type="text/javascript" src="scripts/ECOP_Mobile_PS.appDefinition.js');
-    content = content.replace('<script type="text/javascript" src="scripts/OutSystemsReactWidgets.js', '<script async type="text/javascript" src="scripts/OutSystemsReactWidgets.js');
-    content = content.replace('<script type="text/javascript" src="scripts/ECOP_Mobile_PS.index.js', '<script async type="text/javascript" src="scripts/ECOP_Mobile_PS.index.js')
-    //content = content.replace('<script type="text/javascript" src="scripts/ONEConferenceMobile.appDefinition.js', '<script async type="text/javascript" src="scripts/ONEConferenceMobile.appDefinition.js')
-    content = content.substr(0, content.indexOf('<script type="text/javascript" src="scripts/NullDebugger.js')) + content.substr(content.indexOf('</script>', content.indexOf('<script type="text/javascript" src="scripts/NullDebugger.js')) + 9)
-    fs.writeFileSync(path, content, "utf-8");
-}
-
-function indexJSChanger(path) {
-    let indexjs = fs.readFileSync(path, "utf-8");
-    indexjs = indexjs.replace(', NullDebugger', "");
-    indexjs = indexjs.replace(', "OutSystems/ClientRuntime/NullDebugger"', "");
-    fs.writeFileSync(path, indexjs, 'utf-8');
-}
-
 module.exports = {
     getConfigs,
     readErrorFile,
-    errorFileReplacer,
-    indexReplacer,
-    indexJSChanger
+    errorFileReplacer
 }
